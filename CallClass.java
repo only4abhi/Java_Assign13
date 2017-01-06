213680166fa7d32dbfa455c38efa63ab08fbec06
only4abhi <abhishekchanda.mailbox@gmail.com>
2017-01-06 17:38:53 +0530

    Add files via upload

---
+++ CallClass.java
@@ -0,0 +1,12 @@
+package TestPack2;
+import TestPack1.ProClass;
+
+public class CallClass extends ProClass{
+	
+	public static void main(String args[]){
+		
+		CallClass obj = new CallClass();
+		obj.method();
+	}
+
+}
---
+++ ProClass.java
@@ -0,0 +1,12 @@
+package TestPack1;
+
+public class ProClass {
+
+	int total =100;
+	
+	protected void method(){
+		
+		System.out.println("Total "+total);
+	}
+	
+}
